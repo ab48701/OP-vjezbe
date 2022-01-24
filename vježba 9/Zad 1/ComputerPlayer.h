2e53e6225fca1a59e48f1a13eec39031274d4eca
ab48701 <92825924+ab48701@users.noreply.github.com>
2022-01-24 12:36:35 +0100

    Delete vježba 9/Zad 1 directory

--- vježba 9/Zad 1/ComputerPlayer.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Player.h"
-
-class ComputerPlayer : public Player {
-public:
-	int rand_broj();
-	int computer_move();
-};
--- vježba 9/Zad 1/HumanPlayer.h
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include "Player.h"
-#pragma once
-
-
-class HumanPlayer : public Player {
-public:
-	int rand_num();
-};
