edd379b45868e99369dd3b943a843647a881df43
ab48701 <92825924+ab48701@users.noreply.github.com>
2022-01-24 12:38:10 +0100

    Add files via upload

---
+++ Zad 2/Zad2.cpp
@@ -0,0 +1,22 @@
+﻿#include "Brojac.h"
+#include "Kukac.h"
+#include "Pauk.h"
+#include "Ptica.h"
+#include "Zivotinja.h"
+#include "Ptica.cpp"
+#include "Kukac.cpp"
+#include "Pauk.cpp"
+#include "Brojac.cpp"
+
+
+int main()
+{
+	Golub Golub;
+	Mrav  Mrav;
+	CrnaUdovica CrnaUdovica;
+	Brojac brojac;
+	Brojac.getZivotinja(Golub);
+	Brojac.getZivotinja(Mrav);
+	Brojac.getZivotinja(CrnaUdovica);
+
+}
